ede5a9b0d2bbf8c594c82dc5ddf1cb28ce008b77
Rishab <rishabx0@gmail.com>
2026-07-09 01:30:02 +0530

    just changes

---
+++ 01-CloudShare/src/Layout/DashboardLayout.jsx
@@ -0,0 +1,24 @@
+import CardNav from '@/Components/Landing/CardNav';
+import { useUser } from '@clerk/clerk-react'
+import React from 'react'
+
+const DashboardLayout = ({children}) => {
+  const {user} = useUser();
+
+  return (
+    <div>
+      <CardNav/>
+      {user && (
+        <div className="flex">
+        
+        <div className='max-[1080]:hidden'>
+          side menu
+        </div>
+        <div className="grow mx-5">{children}</div>
+      </div>
+      )}
+    </div>
+  )
+}
+
+export default DashboardLayout
